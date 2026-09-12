01b6439cdf14d72263ffdc19e40a041605dc2d84
sam13999 <s.kessai@live.fr>
2026-09-12 18:46:51 +0200

    Support GitHub Pages routing

--- src/App.tsx
+++ src/App.tsx
@@ -36,7 +36,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename={import.meta.env.BASE_URL}>
         <ScrollToTop />
         <DiagnosticProvider>
           <Routes>
